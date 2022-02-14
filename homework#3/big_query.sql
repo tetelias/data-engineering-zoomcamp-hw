0fc400d9531412afe753b3863acf8cbabe203663
tetelias <29153358+tetelias@users.noreply.github.com>
2022-02-14 18:10:52 +0300

    hw3.complete

--- homework#3/big_query.sql
+++ homework#3/big_query.sql
@@ -10,3 +10,22 @@ SELECT COUNT(1) FROM ultimate-life-338623.ny_taxi_trips.external_fhv_tripdata;
 
 -- Query for Q2
 SELECT COUNT(DISTINCT dispatching_base_num) FROM ultimate-life-338623.ny_taxi_trips.external_fhv_tripdata;
+
+-- Create a partitioned table from external table
+CREATE OR REPLACE TABLE ultimate-life-338623.ny_taxi_trips.fhv_tripdata_partitoned
+PARTITION BY
+  DATE(dropoff_datetime) AS
+SELECT * FROM ultimate-life-338623.ny_taxi_trips.external_fhv_tripdata;
+
+-- Create a partitioned table from external table
+CREATE OR REPLACE TABLE ultimate-life-338623.ny_taxi_trips.fhv_tripdata_partitoned_clustered
+PARTITION BY
+  DATE(dropoff_datetime) 
+  CLUSTER BY dispatching_base_num AS
+SELECT * FROM ultimate-life-338623.ny_taxi_trips.external_fhv_tripdata;
+
+-- Query for Q4s
+SELECT count(*) as trips
+FROM ultimate-life-338623.ny_taxi_trips.fhv_tripdata_partitoned_clustered
+WHERE DATE(dropoff_datetime) BETWEEN '2019-01-01' AND '2019-03-31'
+  AND dispatching_base_num IN ('B00987', 'B02060', 'B02279');
